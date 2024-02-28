05e7d08916389dd35d6dc1db109e13e3024a55d3
AyoubAnej <alyapani9@gmail.com>
2024-02-28 08:18:22 +0100

    fourth commit

--- src/main/java/ma/ayb/patientmvc/Security/SecurityConfig.java
+++ src/main/java/ma/ayb/patientmvc/Security/SecurityConfig.java
@@ -1,2 +1,30 @@
-package ma.ayb.patientmvc.Security;public class SecurityConfig {
+package ma.ayb.patientmvc.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}123").roles("USER").build(),
+                User.withUsername("user2").password("{noop}123").roles("USER").build(),
+                User.withUsername("user3").password("{noop}123").roles("USER", "ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin();
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        return httpSecurity.build();
+    }
 }
